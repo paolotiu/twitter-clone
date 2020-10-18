5c36ce6475d7bfb76dfa601c65da18c94bb78f1d
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-10-18 18:23:49 +0800

    firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyBTlxpFw8xHVNLuNxQoIMKlduZpYXAWaTk',
+    authDomain: 'twitter-clone-e3902.firebaseapp.com',
+    databaseURL: 'https://twitter-clone-e3902.firebaseio.com',
+    projectId: 'twitter-clone-e3902',
+    storageBucket: 'twitter-clone-e3902.appspot.com',
+    messagingSenderId: '603192878710',
+    appId: '1:603192878710:web:29b8709d9ddb355f02a157',
+}
+
+export default firebaseConfig
